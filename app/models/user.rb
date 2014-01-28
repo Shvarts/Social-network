83304badb1a68af5ebd585ad91e3e11a197908d3
Yura <georgi.paliy@gmail.com>
2014-01-28 21:13:52 +0200

    created user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+	validates :firstname, :presence => true,
+			 			  :length => {:in => 2..20}
+
+	validates :lastname,  :presence => true,
+						  :length => {:in => 2..20}
+
+	validates :email,	  :presence => true,
+						  :length => {:in => 2..250},
+						  :uniqueness => true,
+						  :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
+
+	validates :password,  :presence => true,
+						  :length => {:within => 6..40}
 end
