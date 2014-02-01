b1d50ad60821c8d34a8524c970b2d3156503c767
xaktest <xaktest@bk.ru>
2014-02-01 10:59:59 +0200

    edit routes and layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SocialNetworc::Application.routes.draw do
   get "wall/index"
- 
+  get '/creat', to: 'creat#index' , as: 'sign_up'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
