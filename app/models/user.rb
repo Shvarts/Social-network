bafb59f93968e49924dc8c621632481c5da9b89a
Olegek <olegek@olegek-Inspiron-M5110.(none)>
2014-02-02 22:01:58 +0200

    create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ActiveRecord::Base
-	validates :firstname, :presence => true,
-			 			  :length => {:in => 2..20}
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
 
-	validates :lastname,  :presence => true,
-						  :length => {:in => 2..20}
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
 
-	validates :email,	  :presence => true,
-						  :length => {:in => 2..250},
-						  :uniqueness => true,
-						  :format => {:with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  private
 
-	validates :password,  :presence => true,
-						  :length => {:within => 6..40}
-
-	has_attached_file :avatar, :styles => { :large => "500x500>", :display => "200x200#" }, :default_url => "/assets/images/missing_avatar.png"
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "UserPages" do
+	
+  describe "GET /user_pages" do
+	  
+    describe "with valid information" do
+	  
+      describe "after saving the user" do
+      before { click_button submit }
+      let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
+  end
+end
