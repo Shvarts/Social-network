486a5df31e90f73600c4f9b5a5fb8a6083ed3358
Ramonlord <Omeha2@gmail.com>
2014-02-02 15:59:28 +0200

    Create Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SocialNetworc::Application.routes.draw do
+  get "users/profile"
   get "wall/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
