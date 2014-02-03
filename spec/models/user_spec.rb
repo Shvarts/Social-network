b4878a05dc2a9c410db3be02e6d62a968474b22f
Olegek <olegek@olegek-Inspiron-M5110.(none)>
2014-02-03 19:44:08 +0200

    try_to_write_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,10 +6,16 @@ describe User do
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
-
   subject { @user }
+
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+
   describe "remember token" do
     before { @user.save }
+
     its(:remember_token) { should_not be_blank }
+  
   end
 end
