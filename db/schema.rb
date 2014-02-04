51cef7ad55d7ee61ede4c85b8eadda71d084b437
worldintime <worldintime@gmail.com>
2014-02-04 11:10:45 +0200

    modal window

--- db/schema.rb
+++ db/schema.rb
@@ -24,4 +24,13 @@ ActiveRecord::Schema.define(version: 20140129123539) do
     t.datetime "photo_updated_at"
   end
 
+  create_table "users", force: true do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
