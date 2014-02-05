9590bf1993e254f8bfa0620f9bace893ea78a7c2
xaktest <xaktest@bk.ru>
2014-02-05 15:32:56 +0200

    Create model Visit

---
+++ db/migrate/20140205133029_create_visits.rb
@@ -0,0 +1,16 @@
+class CreateVisits < ActiveRecord::Migration
+  def change
+    create_table :visits do |t|
+      t.integer :id
+      t.datetime :created_at
+      t.string :ip
+      t.string :browser_name
+      t.string :browser_version
+      t.string :os_name
+      t.string :os_version
+      t.integer :devise_type
+
+      t.timestamps
+    end
+  end
+end
