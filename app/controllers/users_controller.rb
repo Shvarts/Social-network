bc2a03348f1aaaf77e850941479c00ae44abf836
Ramonlord <Omeha2@gmail.com>
2014-02-07 02:42:31 +0200

    Now viewer show errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
   	private
   		def user_params
-    		params.require(:user).permit(:firstname, :lastname, :email)
+    		params.require(:user).permit(:firstname, :lastname, :email, :password)
   		end
 
 end
