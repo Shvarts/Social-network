087e7ba23c9d712882db97fcd1619812da4ab695
Ramonlord <Omeha2@gmail.com>
2014-02-07 02:18:07 +0200

    Password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,10 @@ class User < ActiveRecord::Base
 						  :format => {:with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 
 	validates :password,  :presence => true,
-						  :length => {:within => 6..40}
+						  :length => {:within => 6..40},
+						  :confirmation =>true
+						  
+	validates_confirmation_of :password, message: "Your password and its checking must be identical"
 
 	has_attached_file :avatar, :styles => { :large => "500x500>", :display => "200x200#" }, :default_url => "/assets/images/missing_avatar.png"
 end
