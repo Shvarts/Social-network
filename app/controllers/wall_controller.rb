374ca0fcf82e5da8cb36d0580e7e7a15eb4ba32a
Yuriy Paliy <georgi.paliy@gmail.com>
2014-02-09 03:43:28 +0200

    post list html/css

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -6,34 +6,34 @@ class WallController < ApplicationController
 	
 	def create
     	@post = Post.new(post_params)
-		@post.save
-
+		  @post.save
   		redirect_to wall_url(@post)
     end
 
-    def show
-		@post = Post.find(params[:id])
+   def show
+	  	@post = Post.find(params[:id])
 	end
 	
 	def edit
-        @post = Post.find(params[:id])
+      @post = Post.find(params[:id])
 	end
 
 	def update
-        @post = Post.find(params[:id])
- 
-    if @post.update(post_params)
-       redirect_to wall_url(@post)
-    else
-       render 'edit'
+      @post = Post.find(params[:id])
+
+    if  @post.update(post_params)
+        redirect_to wall_url(@post)
+
+    else render 'edit'
+
     end
+
 	end
 
 
 	def destroy
         @post = Post.find(params[:id])
   	    @post.destroy
- 
         redirect_to action: :index
 	end
 
