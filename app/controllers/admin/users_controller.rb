e5c999adeafd84620103c2fa386b680f69e52204
Yuriy Paliy <georgi.paliy@gmail.com>
2014-02-09 21:52:09 +0200

    delete actions

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,34 +3,34 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
-  def new 
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    @user.save
-    redirect_to admin_users_path
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
   def destroy
     @user = User.destroy(params[:id])
     redirect_to admin_users_path    
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+#  def new 
+#    @user = User.new
+#  end
 
-  def update
-    @user = User.find(params[:id])
-    @user.update_attributes(user_params)
-    redirect_to admin_users_path
-  end
+#  def create
+#    @user = User.new(user_params)
+#    @user.save
+#    redirect_to admin_users_path
+#  end
+
+#  def show
+#    @user = User.find(params[:id])
+#  end
+
+#  def edit
+#    @user = User.find(params[:id])
+#  end
+  
+#  def update
+#    @user = User.find(params[:id])
+#    @user.update_attributes(user_params)
+#    redirect_to admin_users_path
+#  end
 
   private
     def user_params
