9af491752764bb08a3862a976ec70d2669b24441
mskubenich <misha.skubenich@gmail.com>
2014-02-10 10:37:43 +0200

    add linkks on application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SocialNetworc::Application.routes.draw do
-  get "users/profile"
+
+
   get "wall/index"
-  get 'profile' => 'users#profile';
+  get 'profile' => 'users#profile'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
