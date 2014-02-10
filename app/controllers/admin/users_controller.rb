6a11b1c3aab6ccd165397edde9d12f92b6955b8d
Yuriy Paliy <georgi.paliy@gmail.com>
2014-02-10 17:34:20 +0200

    delete actions and views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,30 +8,6 @@ class Admin::UsersController < ApplicationController
     redirect_to admin_users_path    
   end
 
-#  def new 
-#    @user = User.new
-#  end
-
-#  def create
-#    @user = User.new(user_params)
-#    @user.save
-#    redirect_to admin_users_path
-#  end
-
-#  def show
-#    @user = User.find(params[:id])
-#  end
-
-#  def edit
-#    @user = User.find(params[:id])
-#  end
-  
-#  def update
-#    @user = User.find(params[:id])
-#    @user.update_attributes(user_params)
-#    redirect_to admin_users_path
-#  end
-
   private
     def user_params
       params.require(:user).permit(:firstname, :lastname, :email, :password)
